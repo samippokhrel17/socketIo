c8a654832c2467a8c7e27f18546434009ca01473
samippokhrel17 <samippokhrel17@gmail.com>
2024-02-12 17:06:44 +0545

    about events , broadcast, io.sockets.emit, in genral its about restrictiong users who will get what messages

--- app.js
+++ app.js
@@ -12,15 +12,31 @@ app.get("/", function(req, res) {
     res.sendFile(fileName, options); 
 });
 
-// Socket.IO connection
-io.on("connection", function(socket) { 
-    console.log("a user connected");  
 
+var users = 0;
+
+// Socket.IO connection 
+io.on("connection", function(socket) { 
+    console.log("A User connected");  
+    users++;
+    socket.emit("newuserconnect",{message:' Hii, Welcome Brother' }); //this is for new user who is connected  
+
+    socket.broadcast.emit('newuserconnect',{message: users + "users connected"})
+    
+
+    // io.sockets.emit is for all the users 
+    // io.sockets.emit("broadcast",{message: users+'users connected!' });   //this for broadcasting i.e all the users will get messages
     // setTimeout(() => {
     //     // socket.send("Send Message from server side by prereserved events");
     //     socket.emit("myCustomEvent",{description:'A custom message from serer side'})//to create constom event emit is used to catch in client side
           
     //  }, 3000);
+
+    socket.on('disconnect',function(data){
+        console.log("A user Disconnected");
+        users--;
+        socket.broadcast.emit('newuserconnect',{message: users + "users disconnected"})
+    })
      
 //on works is to catch any events
 
